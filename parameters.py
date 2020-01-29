43fdd0cfc068d8ddfe46af38226e2e1ac55cc3e3
Morgan Hollis <mhollis@ras.ac.uk>
2020-01-29 11:31:22 +0000

    Added parameters file

---
+++ parameters.py
@@ -0,0 +1,19 @@
+# Select journal - must be either "ApJ" or "ApJL"
+journal = "ApJL"
+
+# Number of articles to use from each issue
+num_articles = 30
+
+
+# Input parameter validation
+def valid(journal, num_articles):
+	try:
+		if (int(num_articles) < 1):
+			return False  # fails if num_articles < 1
+	except ValueError:
+		return False  # fails if num_articles not an integer
+
+	if journal not in {'APJ', 'APJL', 'apj', 'apjl'}:
+		return False
+
+	return True
